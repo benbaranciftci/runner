94522536d7cb62b8109e26d32ec253b82a704dcb
benbaranciftci <benbaranciftci@gmail.com>
2024-05-04 21:54:17 +0300

    Initial commit

---
+++ scenes/dino.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const GRAVITY : int = 4000
+const JUMP_SPEED : int = -1500
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		if not get_parent().game_running:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$RunCol.disabled = false
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y = JUMP_SPEED
+				$JumpSound.play()
+			else:
+				$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
+		
+	move_and_slide()
---
+++ scenes/menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn") # Replace with function body.
